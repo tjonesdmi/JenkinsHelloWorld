9ed4cfbf558a9cd6dbc84a401b044394d9af729b
Thomas Jones <tjones@dminc.com>
2024-02-02 10:12:33 -0500

    Added second line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Java, brought to you by Jenkins!");
+        System.out.println("Second line");
     }
 }
